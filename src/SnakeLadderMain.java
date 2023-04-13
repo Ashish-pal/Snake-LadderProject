b3d6154002434073726549637b32cc855d76efaf
Ashish Pal <ak987982@gmail.com>
2023-04-13 14:46:31 +0530

    Added dice number rolled by the single player

--- src/SnakeLadderMain.java
+++ src/SnakeLadderMain.java
@@ -1,6 +1,11 @@
+import java.util.Random;
+
 public class SnakeLadderMain {
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("Snake and Ladder Simulator");
+        Random random = new Random();
+        int diceNumber = random.nextInt(6) + 1;
+        System.out.println(diceNumber);
     }
 }
