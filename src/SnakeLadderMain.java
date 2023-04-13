5c9c0eb088a985d73b2aa5ddced873afbc65a1bb
Ashish Pal <ak987982@gmail.com>
2023-04-13 15:08:31 +0530

    Added UC3 Player Position

--- src/SnakeLadderMain.java
+++ src/SnakeLadderMain.java
@@ -7,5 +7,18 @@ public class SnakeLadderMain {
         Random random = new Random();
         int diceNumber = random.nextInt(6) + 1;
         System.out.println(diceNumber);
+        int option = random.nextInt(3);
+        System.out.print("You got ");
+        switch (option) {
+            case 0:
+                System.out.println("No Play.");
+                break;
+            case 1:
+                System.out.println("a Ladder.");
+                break;
+            case 2:
+                System.out.println("a Snake.");
+                break;
+        }
     }
 }
